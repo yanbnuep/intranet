feb1a10647d691ad122f65a7781e0ecd198b5849
yanbnuep <yanbnuep@gmail.com>
2017-01-09 18:09:30 +0800

    tele

--- public/javascript/pages/telephoneSearch.js
+++ public/javascript/pages/telephoneSearch.js
@@ -1,3 +1,66 @@
 /**
  * Created by itdwyy on 12/13/2016.
  */
+$(document).ready(function () {
+    getConstructor(addDepartment);
+});
+
+function getConstructor(callback) {
+    $.ajax({
+        url: '/tele/construct',
+        method: 'get',
+        success: function (json) {
+            callback(json);
+        }
+    });
+}
+
+function addDepartment(constructor) {
+    var departmenthtml = $(),
+        divhtml = $();
+    for (var department in constructor) {
+        departmenthtml = $('<li><a href="/" class="sidebar-link">' + department + '</a></li>');
+        if (constructor.hasOwnProperty(department)) {
+            (function (department) {
+                departmenthtml.children('a').click(function (event) {
+                    event.preventDefault() ? event.preventDefault() : (event.returnValue = false);
+                    //get html by department
+                    searchByDepartment(department);
+                });
+            })(department)
+        }
+        if (constructor.hasOwnProperty(department) && constructor[department].length > 0) {
+            var subMenu = $('<ul class="menu-root"></ul>');
+            for (var i = 0; i < constructor[department].length; i++) {
+                var div = constructor[department][i];
+                divhtml = $('<li><a class="section-link" data-scroll href="#' + div + '"> ' + div + '</a></li>');
+                subMenu.append(divhtml);
+            }
+            departmenthtml.append(subMenu);
+        }
+
+
+        $('#headQuarter.menu-root').append(departmenthtml);
+    }
+
+}
+
+function searchByDepartment(department) {
+    $.ajax({
+        url:'/tele/department',
+        data: {department:department},
+        method: 'get',
+        success: function (data) {
+            renderPhoneResult(data);
+        }
+    });
+}
+
+function renderPhoneResult(data) {
+    var person = {},
+        div = [];
+    for(var i = 0; i< data.length;i++){
+        person = data[i];
+        console.log(person);
+    }
+}
--- routes/tele.js
+++ routes/tele.js
@@ -4,9 +4,106 @@
 var express = require('express');
 var router = express.Router();
 
+var sql = require('mssql');
+
+var config = {
+    user: 'ian',
+    password: 'airmacau',
+    server: '192.168.110.70',
+    database: 'INTRANET'
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('tele', { title: 'telephone' });
+router.get('/', function (req, res, next) {
+    res.render('tele', {title: 'telephone'});
 });
+
+router.get('/construct', function (req, res, nex) {
+    console.log('start get constructor');
+    try {
+        getConstruction(function (data) {
+            res.send(data);
+        })
+    } catch (e) {
+        console.log('error in get constructor: ' + e);
+    }
+
+});
+
+
+function getConstruction(callback) {
+    var constructor = {},
+        department = '',
+        div = '';
+    sql.connect(config, function (err) {
+        try {
+            new sql.Request().query('SELECT DISTINCT DEPT,DIV FROM [INTRANET].[dbo].[TEL_VW] WHERE LOCATION = \'MFM\' order by DEPT', function (err, record) {
+                if (err) {
+                    console.log('error search department: ' + err);
+                    return err;
+                }
+                try{
+                    for (var i = 0; i < record.length; i++) {
+                        department = record[i]['DEPT'];
+                        div = record[i]['DIV'];
+                        //init department
+                        if (department !== null && constructor[department]=== undefined && div !== undefined) {
+                            constructor[department] = [];
+                            constructor[department].push(div);
+                        }else if( constructor[department] !== undefined && div !== undefined){
+                            constructor[department].push(div);
+                        }
+                    }
+                }catch (e){
+                    console.log('error in create constructor: '+e);
+                }
+                if (typeof callback === 'function') {
+                    callback(constructor);
+                } else {
+                    return constructor;
+                }
+            });
+        } catch (e) {
+            console.log('error in get telephone constructor: ' + e);
+        }
+    })
+}
+
+router.get('/department',function(req,res,next){
+    try{
+        byDepartment(req.query['department'],function (data) {
+            res.send(data);
+        })
+    }catch (e){
+        console.log('error search by department: '+ e);
+    }
+});
+
+
+function byDepartment(department,callback) {
+    var result = [],
+        div = {};
+    console.log('SELECT * FROM [INTRANET].[dbo].[TEL_VW] where DEPT = \''+department+'\' and LOCATION = \'MFM\' order by DIV ');
+    sql.connect(config, function (err){
+        if(err){
+            console.log('error in search by department'+err);
+            return null;
+        }
+        try{
+            new sql.Request().query('SELECT * FROM [INTRANET].[dbo].[TEL_VW] where DEPT = \''+department+'\' and LOCATION = \'MFM\' order by DIV ',function (error,data) {
+                if(error){
+                    console.log('error search by department: '+error);
+                    return null;
+                }
+                if (typeof callback === 'function') {
+                    callback(data);
+                } else {
+                    return data;
+                }
+            });
+        }catch (e){
+            console.log('error in sql search by department'+e);
+        }
+    });
+}
 
 module.exports = router;
